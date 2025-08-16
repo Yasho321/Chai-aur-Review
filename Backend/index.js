ece136151bc8c5025fad3e5e151a6c5148e26a54
Yasho321 <yashovardhans321@gmail.com>
2025-08-16 17:03:29 +0530

    Debugging cookies issue

--- Backend/index.js
+++ Backend/index.js
@@ -41,7 +41,7 @@ app.use(
     }),
     cookie: {
       maxAge: 1000 * 60 * 60 * 24, // 1 day
-      sameSite: "none",
+      sameSite: "None",
       secure: true,
       domain : ".vercel.app"
     },
